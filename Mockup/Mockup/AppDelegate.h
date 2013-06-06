2af3357735e70cef0e32481e9f97fc786fac6567
Neel Patel <npatel@college.harvard.edu>
2013-06-06 15:38:23 -0400

    commit before pulling

---
+++ Mockup/Mockup/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Mockup
+//
+//  Created by Neel Patel on 6/3/13.
+//  Copyright (c) 2013 Neel Patel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
