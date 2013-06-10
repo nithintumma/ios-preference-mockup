d53a13d6c2938478ff2ba06b22a3dfaf32796bf5
Nithin Tumma <ntumma@college.harvard.edu>
2013-06-09 23:36:27 -0700

    facebook works

--- Mockup/LoginViewController.m
+++ Mockup/LoginViewController.m
@@ -22,17 +22,91 @@
     
     // Check if user is cached and linked to Facebook, if so, bypass login
     if ([PFUser currentUser] && [PFFacebookUtils isLinkedWithUser:[PFUser currentUser]]) {
-        NSLog(@"Been here");
         [self performSegueWithIdentifier:@"loginToHomeScreen" sender:self];
     }
-
-	// Do any additional setup after loading the view.
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void) initializeFacebookInformation
+{
+    // get basic user information and 
+    FBRequest *request = [FBRequest requestForMe];
+    FBRequest* friendsRequest = [FBRequest requestForMyFriends];
+    [friendsRequest startWithCompletionHandler: ^(FBRequestConnection *connection,
+                                                  NSDictionary* result,
+                                                  NSError *error) {
+        if (!error) {
+        NSArray* friends = [result objectForKey:@"data"];
+        NSMutableArray *ids = [[NSMutableArray alloc] initWithCapacity: [friends count]];
+        
+        for (NSDictionary<FBGraphUser>* friend in friends) {
+            [ids addObject: friend.id];
+        }
+            
+        [[PFUser currentUser] setObject:ids forKey:@"friends"];
+        [[PFUser currentUser] saveInBackground];
+        } else {
+            NSLog(@"Error while getting facebook friends");
+        }
+        
+    }];
+    [request startWithCompletionHandler:^(FBRequestConnection *connection, id result, NSError *error) {
+        // handle response
+        if (!error) {
+            // Parse the data received
+            NSDictionary *userData = (NSDictionary *)result;
+            
+            NSString *facebookID = userData[@"id"];
+            
+            NSURL *pictureURL = [NSURL URLWithString:[NSString stringWithFormat:@"https://graph.facebook.com/%@/picture?type=large&return_ssl_resources=1", facebookID]];
+            
+            // dict to hold data
+            NSMutableDictionary *userProfile = [NSMutableDictionary dictionaryWithCapacity:7];
+            
+            if (facebookID) {
+                userProfile[@"facebookId"] = facebookID;
+            }
+            
+            if (userData[@"name"]) {
+                userProfile[@"name"] = userData[@"name"];
+            }
+            
+            if (userData[@"location"][@"name"]) {
+                userProfile[@"location"] = userData[@"location"][@"name"];
+            }
+            
+            if (userData[@"gender"]) {
+                userProfile[@"gender"] = userData[@"gender"];
+            }
+            
+            if (userData[@"birthday"]) {
+                userProfile[@"birthday"] = userData[@"birthday"];
+            }
+            
+            if (userData[@"relationship_status"]) {
+                userProfile[@"relationship"] = userData[@"relationship_status"];
+            }
+            
+            if ([pictureURL absoluteString]) {
+                userProfile[@"pictureURL"] = [pictureURL absoluteString];
+            }
+            
+            [[PFUser currentUser] setObject:userProfile forKey:@"profile"];
+            [[PFUser currentUser] saveInBackground];
+            
+        } else if ([[[[error userInfo] objectForKey:@"error"] objectForKey:@"type"]
+                    isEqualToString: @"OAuthException"]) { // Since the request failed, we can check if it was due to an invalid session
+            NSLog(@"The facebook session was invalidated");
+        } else {
+            NSLog(@"Some other error: %@", error);
+        }
+    }];
+
 }
 
 - (IBAction)loginButtonTouch:(id)sender {
@@ -48,25 +122,19 @@
                  UIAlertView *alert = [[UIAlertView alloc] initWithTitle: @"Log in Error"
                                                                  message:@"Uh oh. The user cancelled the Facebook login." delegate:nil cancelButtonTitle:nil otherButtonTitles:@"Dismiss", nil];
                  [alert show];
-                 
              } else {
                  NSLog (@"Error occured");
                  UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Log In Error" message:[error description] delegate:nil cancelButtonTitle:nil otherButtonTitles:@"Dismiss", nil];
                  [alert show];
              }
          } else if (user.isNew) {
-             UIAlertView *alert = [[UIAlertView alloc] initWithTitle: @"New Log In"
-                                                             message:@"Good stuff." delegate:nil cancelButtonTitle:nil otherButtonTitles:@"Dismiss", nil];
-             [alert show];
+             // set up the facebook init
+             [self initializeFacebookInformation];
              [self performSegueWithIdentifier:@"loginToHomeScreen" sender: self];
          } else {
-             UIAlertView *alert = [[UIAlertView alloc] initWithTitle: @"Returning User"
-                                                             message:@"Got it to work" delegate:nil cancelButtonTitle:nil otherButtonTitles:@"Dismiss", nil];
-             [alert show];
+             // returning user should be stored in the database
              [self performSegueWithIdentifier:@"loginToHomeScreen" sender: self];
-             
          }
-         
      }];
     [_activityIndicator startAnimating];
 }
